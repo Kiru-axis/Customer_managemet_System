46b02c191a965693b6fc0b9b042e6dd3533ae517
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-08-02 23:01:04 +0300

    initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns =[
+    path("",views.dashboard,name="dashboard"),
+    path("products/",views.products,name="products"),
+    path("customer/",views.customer,name="customer"),
+    
+]
